240f3e4ca2db90debd7e303b26400f2bc2002c6f
tapas4java <tapas4java@403e3096-1299-4d6c-bf7b-bad521e99d0e>
2012-08-02 10:58:33 +0000

    The following moidifications has been done. 1.Multiple row add alignment issue fixed 2.Focus problem resolved 3.Licence details updated in source files 4.New members are added to about tab 5.Project configuration updated to eclipse version Helios(3.6)

--- trunk/DataGen/src/com/esspl/datagen/util/DataGenConstant.java
+++ trunk/DataGen/src/com/esspl/datagen/util/DataGenConstant.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2012 Enterprise System Solutions (P) Ltd. All rights reserved.
+ *
+ * This file is part of DATA Gen. http://testdatagen.sourceforge.net/
+ *
+ * DATA Gen is a free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DATA Gen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package com.esspl.datagen.util;
 
 import com.vaadin.terminal.ThemeResource;
@@ -110,9 +129,12 @@ public class DataGenConstant {
 	+ "<p><li>Instant Script Runner provided to excute the SQL scripts without using Toad.</li></ul></p>"
 	+ "<h1>Team Members :</h1>"
 	+ "<p><ul><li>Venugopal Prabhoo : venugopal@esspl.com</li>"
-	+ "<li>Santosh D. Kiran &nbsp;&nbsp;&nbsp;: santosh@esspl.com</li>"
+	+ "<li>Santosh D. Kiran &nbsp;&nbsp;&nbsp;&nbsp;: santosh@esspl.com</li>"
 	+ "<li>Tapas Kumar Jena	&nbsp;: tapasj@esspl.com</li>"
 	+ "<li>Anshuman Nayak &nbsp;&nbsp;&nbsp;: anshuman@esspl.com</li>"
-	+ "<li>Basudha Das	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: basudha@esspl.com</li></ul></p>"
+	+ "<li>Basudha Das	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: basudha@esspl.com</li>"
+	+ "<li>Satya P. Mishra &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: satyam@esspl.com</li>"
+	+ "<li>Abhisek Panda &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: abhisek@esspl.com</li>"
+	+ "<li>Nirmal Mandhata &nbsp;&nbsp;: nirmal@esspl.com</li></ul></p>"
 	+ "<p><b>We would welcome feedbacks and suggestion from you.</b></p>";
 }
